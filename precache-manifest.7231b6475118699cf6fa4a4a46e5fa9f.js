5e2c908b69f2a61e095e6f9991e8a6e172f784ce
JadeMM <jade.moksness@gmail.com>
2020-03-25 08:58:59 -0700

    Updates

---
+++ precache-manifest.7231b6475118699cf6fa4a4a46e5fa9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d2c2fff79df91db482acbc4606b90f",
+    "url": "/pizza-orderer/index.html"
+  },
+  {
+    "revision": "b5bc33ab5963fee7ef26",
+    "url": "/pizza-orderer/static/css/main.ebf8e037.chunk.css"
+  },
+  {
+    "revision": "35e895e87fd41ba9e101",
+    "url": "/pizza-orderer/static/js/2.483ba99b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pizza-orderer/static/js/2.483ba99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bc33ab5963fee7ef26",
+    "url": "/pizza-orderer/static/js/main.924b1d19.chunk.js"
+  },
+  {
+    "revision": "1e5230a00943ca782096",
+    "url": "/pizza-orderer/static/js/runtime-main.d34ad772.js"
+  },
+  {
+    "revision": "a04b22277f66356ff25d14e362eb347d",
+    "url": "/pizza-orderer/static/media/pizza.a04b2227.jpg"
+  }
+]);
